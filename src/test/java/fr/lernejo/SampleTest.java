658f77c339ee413103cc02aa4b261cf6a10f3dc7
QuentinBernigaud <bernigaud@et.esiea.fr>
2021-03-16 18:34:30 +0100

    Add test to match 100% coverage

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -1,32 +1,31 @@
 package fr.lernejo;
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class SampleTest {
 
-    private final Sample sample = new Sample();
-
-    @ParameterizedTest
-    @CsvSource({
-        "3, 7, 10",
-        "0, 3, 3",
-    })
-    void add_behaves_as_expected(int a, int b, int expectedResult) {
-        int result = sample.op(Sample.Operation.ADD, a, b);
-
-        assertThat(result).isEqualTo(expectedResult);
-    }
-
 
     @Test
-    void opp_throws_is_operation_is_null() {
-        assertThatExceptionOfType(NullPointerException.class)
-            .isThrownBy(() -> sample.op(null, 1, 3));
+    void add_behaves_as_expected() {
+        int result = new Sample().op(Sample.Operation.ADD,3,3);
+
+        Assertions.assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void mul_behaves_as_expected() {
+        int result = new Sample().op(Sample.Operation.MULT,3, 3);
+
+        Assertions.assertThat(result).isEqualTo(9);
+    }
+
+    @Test
+    void div_behaves_as_expected() {
+        int result = new Sample().op(Sample.Operation.DIV,3, 3);
+
+        Assertions.assertThat(result).isEqualTo(1);
     }
 }
